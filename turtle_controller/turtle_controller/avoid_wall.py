2083984e5eaf4343759a137ea9edd6d8580bc5e3
Shubhabha <shubhankar.kaushik2003@gmail.com>
2024-04-17 00:49:54 +0530

    Avoid Wall Added

--- turtle_controller/turtle_controller/avoid_wall.py
+++ turtle_controller/turtle_controller/avoid_wall.py
@@ -8,7 +8,21 @@ from turtlesim.msg import Pose
 class AvoidWallNode(Node):
     def __init__(self):
         super().__init__("avoid_wall")
+        self.cmd_vel_pub = self.create_publisher(Twist,"/turtle1/cmd_vel", 10)
+        self.pose_sub = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10)
         self.get_logger().info("Avoid Wall started...")
+
+    def pose_callback(self, pose:Pose):
+        cmd = Twist()
+        
+        if pose.x > 9.0 or pose.x < 2.0 or pose.y > 9.0 or pose.y < 2.0:
+            cmd.linear.x = 1.0
+            cmd.angular.z = 0.6
+        else:
+            cmd.linear.x = 5.0
+            cmd.angular.z = 0.0
+        
+        self.cmd_vel_pub.publish(cmd)
 
 
 def main(args = None):
